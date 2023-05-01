7d0d6465b8a73449cab7146515a8b4f097f2fba9
Merbin J Anselm <merbin.j.anselm@sap.com>
2023-05-01 13:10:32 +0530

    fixed minor issues

--- src/handler/webhook/index.ts
+++ src/handler/webhook/index.ts
@@ -21,17 +21,14 @@ export function webhooks(webhookHandlers: AddHandlersParams): Handler {
     });
 
     try {
-      await api.webhooks.process({
+      return api.webhooks.process({
         rawBody: await ctx.req.text(),
         rawRequest: ctx.req,
         rawResponse: ctx.res,
       });
-
-      await logger.info('Webhook processed, returned status code 200');
     } catch (error) {
       await logger.error(`Failed to process webhook: ${error}`);
       throw error;
     }
-    return ctx.res;
   };
 }
